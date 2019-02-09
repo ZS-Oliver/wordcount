5a71feb3a9063c6adfabdf44bb24742cc6cae90d
Oliver <ZS_Oliver@163.com>
2019-02-10 00:51:16 +0800

    单词计数器在线版

---
+++ src/main/java/hadoop/mapreduce/job/WCRunner.java
@@ -0,0 +1,55 @@
+package hadoop.mapreduce.job;
+
+import hadoop.mapreduce.mapper.WCMapper;
+import hadoop.mapreduce.reducer.WCReducer;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+
+/**
+ * 描述一个特定的作业
+ * 指定作业使用哪个map和哪个reduce
+ * 指定作业要处理的数据的路径
+ * 指定作业输出的结果的路径
+ */
+public class WCRunner {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+
+        Configuration conf = new Configuration();
+
+        Job wcJob = Job.getInstance(conf);
+
+        // 设置整个job所用的那些类在哪个jar包
+        wcJob.setJarByClass(WCRunner.class);
+
+
+        // job使用的mapper和reducer类
+        wcJob.setMapperClass(WCMapper.class);
+        wcJob.setReducerClass(WCReducer.class);
+
+        // 指定reduce的输出数据kv类型
+        wcJob.setOutputKeyClass(Text.class);
+        wcJob.setOutputValueClass(LongWritable.class);
+
+        // 指定mapper的输出数据kv类型
+        wcJob.setMapOutputKeyClass(Text.class);
+        wcJob.setMapOutputValueClass(LongWritable.class);
+
+        // 指定输入数据存放路径
+        FileInputFormat.setInputPaths(wcJob, new Path("/wc/data"));
+        // 指定处理结果输出路径
+        FileOutputFormat.setOutputPath(wcJob,new Path("/wc/output"));
+
+        // 将job提交给集群运行,boolean值为true表示显示执行过程
+        wcJob.waitForCompletion(true);
+    }
+
+}
--- src/main/java/hadoop/mapreduce/mapper/WCMapper.java
+++ src/main/java/hadoop/mapreduce/mapper/WCMapper.java
@@ -1,9 +1,30 @@
 package hadoop.mapreduce.mapper;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-// 前两个指定mapper输入数据的类型，KEYIN为输入的key类型，VALUEYIN是输入的value的类型
-//
-public class WCMapper extends Mapper {
+import java.io.IOException;
 
+// 前两个指定mapper输入数据的类型，KEYIN为输入的key类型，VALUEYIN是输入的value的类型
+// 后两个指定mapper输出的类型，KEYOUT为输出的key类型，VALUEOUT为输出的value类型
+// map和reduce的数据输入输出都是以key-value对的形式封装的
+
+// LongWriteable为hadoop为减小序列化后大小所对Long做的封装，同理，Text对应String
+public class WCMapper extends Mapper<LongWritable, Text,Text,LongWritable> {
+
+    // mapreduce框架每读一次数据就调用一次该方法
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+        // key是这一行数据的起始偏移量，value是这一行的文本内容
+        String line = value.toString();
+
+        String[] words = StringUtils.split(line, " ");
+
+        for(String word : words){
+            context.write(new Text(word),new LongWritable(1));
+        }
+    }
 }
